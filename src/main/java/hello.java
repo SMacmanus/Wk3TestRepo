7d57a604565fabb9fc9048ea04c43ab204fc1031
SMacmanus <brinamac24@gmail.com>
2019-07-08 09:38:16 -0500

    second commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,5 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("HELLO WORLD"); //added for second commit in gitBash exercise 7/8/19
     }
+
 }
